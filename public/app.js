c3694e3da2b2e056e0c6bad382826f29769b818e
Dick H Larsson <dicklarsson@gmail.com>
2022-09-06 12:52:54 +0200

    Change link to sign in to avoid popup blockers on Safari

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ async function pollStatus() {
   const asdf = await fetch("/authenticated");
   const data = await asdf.json();
 
-  if(data && data.hintCode === "started"){
+  if (data && data.hintCode === "started") {
     document.getElementById("app").innerHTML = "<h2>in progress</h2>";
   }
   if (data.authenticated) {
@@ -18,10 +18,8 @@ const anonymousInterval = setInterval(pollStatus, 2000);
 function authenticated(data) {
   //No need to check authentication status no more
   clearInterval(anonymousInterval);
-
   const dom = document.getElementById("app");
-  dom.innerHTML = `
-
+  dom.innerHTML = ` 
    <h1>Weeee you are athenticated <code id="nft">${data.nft}</code></h1>
     <button id="buttonSignOut">Sign out</button>
     `;
@@ -34,36 +32,30 @@ function authenticated(data) {
       window.location.reload();
     });
 }
-function anonymous() {
+async function anonymous() {
   const dom = document.getElementById("app");
   dom.innerHTML = `
   <h1>Members only</h1>
   <div>
       <button id="buttonSignIn" class="btn btn-primary">Click to sign in</button>
-  </div>
-  
+  </div> 
   `;
 
-  document
-    .getElementById("buttonSignIn")
-    .addEventListener("click", async function (event) {
-      //Create a sign in request
-      //Open a new sign in window.
+  const URL = "/signin/step1";
 
-      const URL = "/signin/step1";
+  const asdf = await fetch(URL);
+  const data = await asdf.json();
+  const signInURL = data.signInURL;
+  const orderRef = data.orderRef;
 
-      const asdf = await fetch(URL);
-      const data = await asdf.json();
-      const signInURL = data.signInURL;
-      const orderRef = data.orderRef;
-
-      window.open(signInURL);
-      dom.innerHTML = `
-      
+  dom.innerHTML = ` 
       <h2>Sign in on this device</h2>
-      <div id="status">Signing in</div>
-      
+      To sign in on this device click <a target="_blank" class="button" href="${signInURL}">HERE</a>
+
+      <h2>Sign in on your mobile device</h2>
+   
+      <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${signInURL}"/>
+
+      <div id="status"></div> 
       `;
-      //window.open(data.signInURL);
-    });
 }
